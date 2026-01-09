43161fe8c6bfc69c0e1c3a9b1deb23804b612015
Kendrick Wilkerson <Kendrickwilkerson22@gmail.com>
2026-01-08 20:56:27 -0500

    Initial setup: React + TypeScript + Bootstrap

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      {/* Navbar */}
+      <div>Navbar</div>
+
+      {/* Sections */}
+      <section id="about">About</section>
+      <section id="skills">Skills</section>
+      <section id="projects">Projects</section>
+      <section id="resume">Resume</section>
+      <section id="contact">Contact</section>
+    </>
+  );
+}
+
+export default App;
